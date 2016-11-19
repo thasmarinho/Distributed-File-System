da9bcace77fc9ceb941c57048fe7eca615b93af2
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-11-19 20:57:16 -0300

    Update Principal.java
    included MD5 hash System

--- Principal.java
+++ Principal.java
@@ -1,12 +1,16 @@
 import java.io.IOException;
- 
+
 public class Principal {
  
     public static void main(String args[]) throws IOException {
-        String path = "C:/Users/Thais/Documents/GitHub/ExclusaoMutua/ExclusaoMutua/src/Mapa.txt";
- 
-        //MapaDeReplicacao.write(path);
-        MapaDeReplicacao.read(path);
+        String path = "C:\\Users\\Bruno Augusto\\Desktop\\testeTXT\\mapa.txt";
+        String MD5Result;
+        String Partition;
+        //Proxy.write(path);
+        MD5Result=Proxy.md5("foto.png");
+        System.out.println(MD5Result);
+        Proxy.read(path);
+        Partition=Proxy.Search(path,MD5Result);
     }
  
 }
