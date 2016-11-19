8c84ad81bdd85a2e0db0ed73f4b43c82a76c5575
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-11-19 20:58:37 -0300

    added the MD5 hash

--- Proxy.java
+++ Proxy.java
@@ -3,8 +3,25 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Scanner;
+import java.security.*;
  
 public class Proxy extends MapaDeReplicacao{
+	
+	public static String md5(String senha){
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		BigInteger hash = new BigInteger(1, md.digest(senha.getBytes()));
+		float se = hash.floatValue();
+		float sr=se%3;
+		int t=(int)sr;
+		String s =Integer.toString(t);
+		return s;
+	}
 
 }
