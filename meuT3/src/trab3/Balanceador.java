e51113b86526fc02b6efece07f5dad67fd08b250
BrunoAugustoPereira <bruno.augusto.pp@gmail.com>
2016-11-24 08:23:35 -0300

    Update and rename BalanceadorImplement.java to Balanceador.java

--- meuT3/src/trab3/Balanceador.java
+++ meuT3/src/trab3/Balanceador.java
@@ -4,7 +4,7 @@ import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface BalanceadorImplement extends Remote   {
+public interface Balanceador extends Remote   {
 
 		
 	int getProxy() throws RemoteException, NotBoundException;
